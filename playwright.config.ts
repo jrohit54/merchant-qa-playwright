cb24a887ad153696a1fbe9208c9547220fb48b82
rohit jain <rohjain@groupon.com>
2025-07-03 00:11:07 +0530

    first commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 600000,  
+  timeout: 60000, // 60 seconds
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
