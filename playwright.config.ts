5176ee5b050ddb841cec393f5f122f311557808a
rohit jain <rohjain@groupon.com>
2025-07-02 23:54:58 +0530

    added the headless as true

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     trace: 'on',
     screenshot:'on',
     video:'on',
-    headless: false
+    headless: true
   },
 
   /* Configure projects for major browsers */
